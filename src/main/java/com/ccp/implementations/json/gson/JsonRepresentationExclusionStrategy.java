feadb9754fe5ae05ad5128981daca4b11f392995
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-06-12 01:24:04 -0300

    refatoraçoes

--- src/main/java/com/ccp/implementations/json/gson/JsonRepresentationExclusionStrategy.java
+++ src/main/java/com/ccp/implementations/json/gson/JsonRepresentationExclusionStrategy.java
@@ -2,6 +2,7 @@ package com.ccp.implementations.json.gson;
 
 import java.lang.reflect.Type;
 
+import com.ccp.business.CcpBusiness;
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 
@@ -13,10 +14,27 @@ class JsonRepresentationExclusionStrategy implements ExclusionStrategy{
 	
 	
 	public boolean shouldSkipField(FieldAttributes f) {
+		boolean startsWith = this.skip(f, Class.class, CcpBusiness.class);
+		return startsWith;
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private boolean skip(FieldAttributes f, Class... class1) {
+		for (Class class2 : class1) {
+			boolean skip = this.skip(f, class2);
+			if(skip) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@SuppressWarnings("rawtypes")
+	private boolean skip(FieldAttributes f, Class<Class> class1) {
 		Type declaredType = f.getDeclaredType();
 		
 		String typeName = declaredType.getTypeName();
-		String name = Class.class.getName();
+		String name = class1.getName();
 		boolean startsWith = typeName.startsWith(name);
 		return startsWith;
 	}
